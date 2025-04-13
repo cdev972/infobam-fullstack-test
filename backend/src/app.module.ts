d71315f129370733551299587cb0b11b5b8c669b
cdev <cdev972.project@gmail.com>
2025-04-13 15:53:52 +0200

    Ajout communication entre le back et le front

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { VehiclesController } from './vehicles/vehicles.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [VehiclesController],
 })
-export class AppModule {}
+export class AppModule { }
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Activer CORS pour permettre les requêtes du frontend
+  app.enableCors();
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
